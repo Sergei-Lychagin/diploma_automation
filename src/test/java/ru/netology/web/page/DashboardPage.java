81d95c9c82655bcadcbd87518b64862e4dd37e7c
Sergei_Lychagin <lychagin0@gmail.com>
2021-07-28 10:52:51 +0300

    добавлен page

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,30 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$$;
+
+
+public class DashboardPage {
+    private final SelenideElement paymentButton = $$("button").find(exactText("Купить"));
+    private final SelenideElement creditButton = $$("button").find(exactText("Купить в кредит"));
+    private final SelenideElement payCard = $$("h3[class]").find(exactText("Оплата по карте"));
+    private final SelenideElement payCredit = $$("h3[class]").find(exactText("Кредит по данным карты"));
+
+    public PaymentPage payByDebitCard() {
+        paymentButton.click();
+        payCard.waitUntil(enabled, 15000);
+        return new PaymentPage();
+    }
+
+    public PaymentPage payByCreditCard() {
+        creditButton.click();
+        payCredit.waitUntil(enabled, 15000);
+        return new PaymentPage();
+    }
+}
